c9a95437b3ef34b9aec578f3d0948c0a90700032
Karlen95h <karlen.h.1995@gmail.com>
2024-05-11 13:28:52 +0400

    data

--- audioPlayerKarlen/script.js
+++ audioPlayerKarlen/script.js
@@ -1,5 +1,27 @@
-//Test
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+var data = {
+    title: [
+        'polo-pan-canopee',
+        "Nemra - Nare",
+        "Billie Eilish - everything i wanted",
+        "Claud Debussy - Reverie",
+        "Tigran Hamasyan - Leaving Paris",
+        "Freddie Mercury - Mamma"
+    ],
+    song: [
+        'music/polo-pan-canopee.mp3',
+        "music/Nemra - Nare.mp3",
+        "music/Billie Eilish - everything i wanted.mp3",
+        "music/Debussy Reverie.mp3",
+        "music/Tigran Hamasyan Leaving Paris.mp3",
+        "music/Freddie Mercury - Mamma (Bohemian Rhapsody).mp3"
+    ],
+    poster: [
+        "https://i.gifer.com/7d20.gif",
+        "https://media2.giphy.com/media/5xtDarmSceWsGyD4VeE/source.gif",
+        "https://cdn.dribbble.com/users/1242979/screenshots/7099165/music.gif",
+        "https://i.gifer.com/8RWM.gif",
+        "https://media2.giphy.com/media/Y0GyZQpjqafoatTvjB/giphy.gif",
+        "https://cdn.dribbble.com/users/1921422/screenshots/5511883/freddie.gif"
+    ]
 }
+
